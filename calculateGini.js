75cb5aea26251b5b8c99d9cccac919ac60725e8f
QiuQiu <qiu@peachq.com>
2024-01-01 17:18:01 +0800

    set gitignore and prettier

--- calculateGini.js
+++ calculateGini.js
@@ -18,48 +18,49 @@ let people = 100;
 let turn = 1000000;
 
 function experiment() {
-// 首先，把所有人放进一个数组里并给每个人发100元
-    let wealth = new Array(people);
-    wealth.fill(100);
-// console.log(wealth);
-// 第二步，开始进行给钱的轮次
-    for (let a = 0; a < turn; a++) {
-        // 首先要判断这个人有没有钱
-        let hasMoney = new Array(people);
-        hasMoney.fill(false);
-        // console.log(hasMoney);
-        for (let i = 0; i < people; i++) {
-            wealth[i] > 0 ? hasMoney[i] = true : hasMoney[i] = false
-        }
-        // console.log('>>> hasMoney', hasMoney)
+  // 首先，把所有人放进一个数组里并给每个人发100元
+  let wealth = new Array(people);
+  wealth.fill(100);
 
-        // 每个人在这一轮中，依次开始进行给钱的操作
-        for (let p = 0; p < people; p++) {
-            // 如果这个人有钱，则需要给除了自己以外的任意一个人1元
-            let receive = p;
-            // console.log('>>> receive', receive);
-            if (hasMoney[p]) {
-                do {
-                    receive = Math.trunc(Math.random() * people);
-                    // console.log('>>>receive.random', receive)
-                } while (receive === p);
-                wealth[p]--;
-                wealth[receive]++;
-            }
-        }
+  // console.log(wealth);
+  // 第二步，开始进行给钱的轮次
+  for (let a = 0; a < turn; a++) {
+    // 首先要判断这个人有没有钱
+    let hasMoney = new Array(people);
+    hasMoney.fill(false);
+    // console.log(hasMoney);
+    for (let i = 0; i < people; i++) {
+      wealth[i] > 0 ? (hasMoney[i] = true) : (hasMoney[i] = false);
     }
-    let sumOfDifferences = 0;
-    let sumOfWealth = 0;
-    for (let b = 0; b < people; b++) {
-        for (let c = 0; c < people; c++) {
-            sumOfDifferences += Math.abs(wealth[b] - wealth[c])
-        }
-        sumOfWealth += wealth[b]
+    // console.log('>>> hasMoney', hasMoney)
+
+    // 每个人在这一轮中，依次开始进行给钱的操作
+    for (let p = 0; p < people; p++) {
+      // 如果这个人有钱，则需要给除了自己以外的任意一个人1元
+      let receive = p;
+      // console.log('>>> receive', receive);
+      if (hasMoney[p]) {
+        do {
+          receive = Math.trunc(Math.random() * people);
+          // console.log('>>>receive.random', receive)
+        } while (receive === p);
+        wealth[p]--;
+        wealth[receive]++;
+      }
     }
-    let jini = sumOfDifferences / (2 * people * sumOfWealth)
-    console.log('>>> 基尼:', jini)
+  }
+  let sumOfDifferences = 0;
+  let sumOfWealth = 0;
+  for (let b = 0; b < people; b++) {
+    for (let c = 0; c < people; c++) {
+      sumOfDifferences += Math.abs(wealth[b] - wealth[c]);
+    }
+    sumOfWealth += wealth[b];
+  }
+  let jini = sumOfDifferences / (2 * people * sumOfWealth);
+  console.log(">>> 基尼:", jini);
 }
 
 experiment();
 
-// ji'ne = ｜差值｜之和 / 2 * 人数 * 财富总和
+// Gini = ｜差值｜之和 / 2 * 人数 * 财富总和
